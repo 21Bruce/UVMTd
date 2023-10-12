352ffaf824dbb1fe93192536012c0f468fb24a52
21Bruce <brucejagid@gmail.com>
2023-10-11 23:19:06 -0400

    Improved display script

--- dispd.sh
+++ dispd.sh
@@ -2,4 +2,16 @@
 
 # Simple script to print info about daemons
 
-ps -A | grep "UVMTd"
+#ps -A | grep "UVMTd:"
+
+ps -A | awk '
+BEGIN { }
+{
+	if ($1 == "PID") {
+		print $0
+	} else if ($5 == "uvmtd:"){
+		print $0
+	}
+}
+END { }
+'
